4795857f0aeb93faf32de45332c4e44a0eabf6a4
axelg1993 <axelg1993@gmail.com>
2016-03-16 00:41:15 -0400

    H.w is done

--- Parse-Instagramza/AppDelegate.swift
+++ Parse-Instagramza/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.initializeWithConfiguration(
+            ParseClientConfiguration(block: { (configuration:ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "InstaParse"
+                configuration.clientKey = "Barcelona14!"
+                configuration.server = "https://calm-journey-62404.herokuapp.com/parse"
+            })
+        )
         return true
     }
 
